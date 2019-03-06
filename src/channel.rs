d213f375fdd5da15c9d56229a840066ccd3dc5fa
4lDO2 <4lDO2@protonmail.com>
2019-03-06 12:09:57 +0100

    Implement ChannelSend and ChannelRecv for std channels.

--- src/channel.rs
+++ src/channel.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc::{Sender as StdSender, Receiver as StdReceiver, SendError as StdSendError, RecvError as StdRecvError};
+
 pub trait ChannelSend<T> {
     type Error;
     fn send(&mut self, value: &T) -> Result<(), Self::Error>;
@@ -5,4 +7,19 @@ pub trait ChannelSend<T> {
 pub trait ChannelRecv<T> {
     type Error;
     fn recv(&mut self) -> Result<T, Self::Error>;
+}
+
+impl<T: Clone> ChannelSend<T> for StdSender<T> {
+    type Error = StdSendError<T>;
+
+    fn send(&mut self, value: &T) -> Result<(), Self::Error> {
+        StdSender::send(self, value.clone())
+    }
+}
+impl<T> ChannelRecv<T> for StdReceiver<T> {
+    type Error = StdRecvError;
+
+    fn recv(&mut self) -> Result<T, Self::Error> {
+        StdReceiver::recv(self)
+    }
 }
