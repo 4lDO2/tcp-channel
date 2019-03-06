77c63538d6bc3ee6a539968f6d138a6ba524f3b7
4lDO2 <4lDO2@protonmail.com>
2019-03-06 17:21:04 +0100

    Add an integration test, which is a fake time server.

---
+++ tests/time.rs
@@ -0,0 +1,96 @@
+extern crate tcp_channel;
+#[macro_use] extern crate quick_error;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+
+use std::any::Any;
+use std::net::{TcpListener, TcpStream};
+use std::thread::JoinHandle;
+// Yeah, regular channels are used to tell the client when the server has started!
+use std::time::{SystemTime, Duration};
+
+use tcp_channel::{Sender, Receiver, ChannelSend, ChannelRecv};
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+enum Request {
+    RequestTime,
+    SetTime(SystemTime),
+    Stop,
+}
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+enum Response {
+    Respond(SystemTime),
+}
+
+quick_error! {
+    #[derive(Debug)]
+    enum Error {
+        Io(err: std::io::Error) {
+            from()
+        }
+        SendErr(err: std::sync::mpsc::SendError<()>) {
+            from()
+        }
+        RecvErr(err: std::sync::mpsc::RecvError) {
+            from()
+        }
+        SendErr2(err: tcp_channel::SendError) {
+            from()
+        }
+        RecvErr2(err: tcp_channel::RecvError) {
+            from()
+        }
+        JoinErr(err: Box<Any + Send + 'static>) {
+            from()
+        }
+    }
+}
+
+#[test]
+fn time() -> Result<(), Error> {
+    // This test sets up a simple time server, enum based.
+    let initial_time = SystemTime::now();
+    
+    let (sender, receiver) = std::sync::mpsc::channel();
+
+    let thread: JoinHandle<Result<(), Error>> = std::thread::spawn(move || {
+        let mut time = initial_time;
+        let listener = TcpListener::bind("::1:8888")?;
+        sender.send(())?;
+        let (stream, _) = listener.accept()?;
+
+        let mut receiver = Receiver::<Request>::new(stream.try_clone()?);
+        let mut sender = Sender::<Response>::new(stream);
+
+        while let Ok(command) = receiver.recv() {
+            match command {
+                Request::RequestTime => sender.send(&Response::Respond(time))?,
+                Request::SetTime(new_time) => time = new_time,
+                Request::Stop => return Ok(())
+            }
+        }
+
+        Ok(())
+    });
+    dbg!("-1");
+    receiver.recv()?;
+    dbg!("0");
+    let stream = TcpStream::connect("::1:8888")?;
+    let mut sender = Sender::<Request>::new(stream.try_clone()?);
+    let mut receiver = Receiver::<Response>::new(stream);
+
+    sender.send(&Request::RequestTime)?;
+    assert_eq!(receiver.recv()?, Response::Respond(initial_time));
+    let new_time = initial_time + Duration::from_secs(42);
+
+    sender.send(&Request::SetTime(new_time))?;
+    sender.send(&Request::RequestTime)?;
+    assert_eq!(receiver.recv()?, Response::Respond(new_time));
+
+    sender.send(&Request::Stop)?;
+
+    thread.join()??;
+
+    Ok(())
+}
